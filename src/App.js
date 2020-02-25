482ae9d74514a983e4a9dccc9669aa3b8f0beb9c
ConnieFisher <ConnieFisher@users.noreply.github.com>
2020-02-25 17:08:23 -0500

    End of Lesson 7

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import './App.css';
 import WeatherCard from './components/WeatherCard/component';
 
 function App() {
+const data = async () => {
+  const apiRes = await fetch(
+    "https://api.darksky.net/forecast/65dcb962172561d54e1a24fec550cb63/35.1074829,-81.074356"
+    
+  );
+  const resJSON= await apiRes.json();
+  return resJSON;
+};
+
+data().then(res=>console.log(res.currently.temperature));
+
+
   return (
     <div className="App">
-      <WeatherCard temp = {10} />
-      <WeatherCard temp = {22} />
-      <WeatherCard temp = {-15} />
+      <WeatherCard temp = {10} condition='sleet' city='London' country='UK'/>
+      <WeatherCard temp = {22} condition='rain' city='Portland' country='US'/>
+      <WeatherCard temp = {-15} condition='snow' city='Berlin' country='DE'/>
     </div>
   );
 }
--- src/components/WeatherCard/Condition.js
+++ src/components/WeatherCard/Condition.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-const Condition = props => {
+
+const Condition = ({temp, condition}) => {
   const Temp = styled.h1`
     font-family: 'Fira Sans', sans-serif;
     font-size: 2rem;
     font-weight: 200;
   `;
-  const Condition = styled.h3`
+  const State = styled.h3`
     font-family: 'Merriweather', sans-serif;
     font-size: 1.2rem;
   `;
 
   return (
     <>
-      <Temp>20 °C</Temp>
-      <Condition>Clear</Condition>
+      <Temp>{temp} °C</Temp>
+      <State>{condition}</State>
     </>
   );
 };
